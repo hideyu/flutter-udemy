690d7c987c9a3da42fe91b2d77880c2eb4ec8262
hideyu <hsuzuki410@gmail.com>
2021-01-20 11:36:04 +0900

    fix(flash-chat*): fail conection with firebase iOS

---
+++ flash-chat-flutter_old/ios/Runner/AppDelegate.swift
@@ -0,0 +1,31 @@
+import UIKit
+import Flutter
+import Firebase  // 追加
+import FirebaseCore  // 追加
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()  // 追加
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+// import UIKit
+// import Firebase
+
+// @UIApplicationMain
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+
+//   var window: UIWindow?
+
+//   func application(_ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions:
+//       [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+//     FirebaseApp.configure()
+//     return true
+//   }
---
+++ flash-chat-flutter_old/lib/screens/welcome_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+// ticker providerの機能を持たせるためにwith SingleTickerProviderStateMixinを追加する
+// アニメーションを複数扱う場合はTickerProviderMixinを使う
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    );
+    animation =
+        ColorTween(begin: Colors.black, end: Colors.white).animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {}); // animation controllerの値は変わっていくのでsetstateの中に記述する必要なし
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.red.withOpacity(controller.value),
+      // backgroundColor: animation.value,
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              title: 'Login',
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                //Go to login screen.
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            RoundedButton(
+              title: 'Registration',
+              color: Colors.blueAccent,
+              onPressed: () {
+                //Go to login screen.
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

3c51f668f3e43a2320be3f693a5cdfa4e4679614
hideyu <hsuzuki410@gmail.com>
2021-01-21 22:39:46 +0900

    feat(flash_chat_new/*): firebase connections

--- flash_chat_new/lib/main.dart
+++ flash_chat_new/lib/main.dart
@@ -9,17 +9,18 @@ import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/welcome_screen.dart';
 
-void main() => runApp(FlashChat());
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: ThemeData.dark().copyWith(
-      //   textTheme: TextTheme(
-      //     body1: TextStyle(color: Colors.black54),
-      //   ),
-      // ),
       home: WelcomeScreen(),
       initialRoute: WelcomeScreen.id,
       routes: {
--- flash_chat_new/lib/screens/chat_screen.dart
+++ flash_chat_new/lib/screens/chat_screen.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat_new/constants.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
-
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore
+      .instance; // latest versionではFirestore -> FirebaseFirestoreに変更になってる
+  User loggedInUser; // latest versionではFirebaseUser -> Userに変更になってる
+  String messageText;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    // userがsigninしているかどうかを確認する
+    try {
+      final user = _auth.currentUser; // latest versionではメソッドじゃなくてプロパティになった
+      // final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getMessages() async {
+    // Udemyと書き方が変わっているので注意
+    final messages = await _firestore
+        .collection('messages')
+        .get(); // messagesにはFuture<Querysnapshot>が入る
+    for (var message in messages.docs) {
+      // messages.docsにはDocumentSnapshotのListが格納されている
+      print(message.data()); // message.data()で各データにアクセスできる
+    }
+  }
+
+  void messagesStream() async {
+    // Futureオブジェクトのリスト(Stream)が格納する
+    Stream<QuerySnapshot> snapshots =
+        _firestore.collection('messages').snapshots();
+    await for (var snapshot in snapshots) {
+      print(snapshot);
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +71,10 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 //Implement logout functionality
+                // _auth.signOut();
+                // Navigator.pop(context);
+                // getMessages();
+                messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
@@ -38,6 +94,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     child: TextField(
                       onChanged: (value) {
                         //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
@@ -45,6 +102,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   FlatButton(
                     onPressed: () {
                       //Implement send functionality.
+                      //messageText + loggedInUser.email
+                      _firestore.collection('messages').add(
+                        {
+                          'text': messageText,
+                          'sender': loggedInUser.email,
+                        },
+                      );
                     },
                     child: Text(
                       'Send',
